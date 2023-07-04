f9e1562bfccc8bd288b502dfd273362190c65eee
José Luis de la Hoz García <jl@joseluis.io>
2023-07-04 13:46:36 +0200

    Fibonacci recursivo actualizados

--- fibonacci-recursivo/fibonacci.cpp
+++ fibonacci-recursivo/fibonacci.cpp
@@ -1,9 +1,4 @@
-int fibonacci(int n){
-  if(n == 0){
-    return 0;
-  }else if(n == 1){
-    return 1;
-  }else{
-    return (fibonacci(n-1) + fibonacci(n-2));
-  }
+int fib(int n){
+  if (n <= 1) return n;
+  return fib(n - 1) + fib(n - 2);
 }
--- fibonacci-recursivo/fibonacci.js
+++ fibonacci-recursivo/fibonacci.js
@@ -1,9 +1,6 @@
-function fibonacci(n){
-    if(n === 0){
-	return 0;
-    }else if(n === 1){
-	return 1;
-    }else{
-	return (fibonacci(n-1) + fibonacci(n-2));
-    }
-}
+const fib = (n) => {
+    if (n <= 1) return n;
+    return fib(n - 1) + fib(n - 2);
+};
+
+export default fib;
