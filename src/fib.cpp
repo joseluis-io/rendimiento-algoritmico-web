3fdadb1d209700cc82bdb5d29b23dabe2785cc57
José Luis de la Hoz García <jl@joseluis.io>
2023-08-17 19:16:17 +0200

    Fibonacci recursivo javascript en navegador
    Creación de una simple interfaz de usuario para ejecutar los códigos
    en JavaScript y WASM.

--- src/fib.cpp
+++ src/fib.cpp
@@ -1,6 +1,12 @@
 #include <emscripten.h>
 
-EMSCRIPTEN_KEEPALIVE
+#ifdef __cplusplus
+#define EXTERN extern "C"
+#else
+#define EXTERN
+#endif
+
+EXTERN EMSCRIPTEN_KEEPALIVE
 int fib(int n){
   if (n <= 1) return n;
   return fib(n - 1) + fib(n - 2);
