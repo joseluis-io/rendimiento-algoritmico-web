626369019eb80669fa5b03b39f3e80d1e6f16f29
José Luis de la Hoz García <jl@joseluis.io>
2023-11-02 19:33:56 +0100

    Deno/Rust: varias funciones en un ejecutable (prueba resta)

--- src/deno_ffi/algorithm.rs
+++ src/deno_ffi/algorithm.rs
@@ -2,3 +2,8 @@
 pub extern "C" fn add(a: isize, b: isize) -> isize {
     a + b
 }
+
+#[no_mangle]
+pub extern "C" fn sub(a: isize, b: isize) -> isize {
+    a - b
+}
--- src/deno_ffi/main.ts
+++ src/deno_ffi/main.ts
@@ -4,11 +4,14 @@ const libName = `./libalgorithm.so`;
 const dylib = Deno.dlopen(
   libName,
   {
-    "add": { parameters: ["isize", "isize"], result: "isize" },
+      "add": { parameters: ["isize", "isize"], result: "isize" },
+      "sub": { parameters: ["isize", "isize"], result: "isize" },
   } as const,
 );
 
 // Call the symbol `add`
 const result = dylib.symbols.add(35, 34); // 69
+const subResult = dylib.symbols.sub(4, 2); // 2
 
 console.log(`Result from external addition of 35 and 34: ${result}`);
+console.log(`Resta externa de 4 y 2: ${subResult}`);
