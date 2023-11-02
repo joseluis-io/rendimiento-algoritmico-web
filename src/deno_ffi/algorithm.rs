b20d7845c09c7de15ea5f9921cd98a31c6511196
José Luis de la Hoz García <jl@joseluis.io>
2023-11-02 20:12:18 +0100

    Deno/Rust: Fibonacci recursivo

--- src/deno_ffi/algorithm.rs
+++ src/deno_ffi/algorithm.rs
@@ -7,3 +7,11 @@ pub extern "C" fn add(a: isize, b: isize) -> isize {
 pub extern "C" fn sub(a: isize, b: isize) -> isize {
     a - b
 }
+
+#[no_mangle]
+pub extern "C" fn fib(n: isize) -> isize {
+    match n {
+        0 | 1 => n,
+        _ => fib(n - 1) + fib(n - 2),
+    }
+}
--- src/deno_ffi/main.ts
+++ src/deno_ffi/main.ts
@@ -6,12 +6,16 @@ const dylib = Deno.dlopen(
   {
       "add": { parameters: ["isize", "isize"], result: "isize" },
       "sub": { parameters: ["isize", "isize"], result: "isize" },
+      "fib": { parameters: ["isize"], result: "isize" },
   } as const,
 );
 
 // Call the symbol `add`
 const result = dylib.symbols.add(35, 34); // 69
 const subResult = dylib.symbols.sub(4, 2); // 2
+let fib = parseInt(Deno.args[0], 10);
+const fibResult = dylib.symbols.fib(fib);
 
 console.log(`Result from external addition of 35 and 34: ${result}`);
 console.log(`Resta externa de 4 y 2: ${subResult}`);
+console.log(`fib(${fib})=${fibResult}`);
