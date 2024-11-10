4e361530f4cdb3302108d9e00b231a1d2fa397f6
José Luis de la Hoz <jl@joseluis.io>
2024-11-10 21:27:34 +0100

    Benchmark deno-rust: fibonacci

--- src/deno-addons/main.ts
+++ src/deno-addons/main.ts
@@ -11,13 +11,52 @@ const dylib = Deno.dlopen(
   } as const,
 );
 
-
-
 const { fib, linearSearch, binarySearch, bubbleSort } = dylib.symbols;
+import { Queue } from './queue.js';
 
-import {Queue} from './queue.js';
+function benchmark(algorithm, inputs) {
+  const results = [];
+  for (const input of inputs) {
+    const start = performance.now();
+    algorithm(input);
+    const end = performance.now();
+    results.push({
+      algorithm: algorithm.name,
+      input: input,
+      time: end - start
+    });
+  }
+  return results;
+}
 
-let cola = new Queue();
-cola.push(3);
-console.log(cola.peek() === 3);
-console.log(cola.isEmpty());
+// formato: YYYYMMDDTHHMM
+function getCurrentDateTime() {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  return `${year}${month}${day}T${hours}${minutes}`;
+}
+
+async function exportToCSV(results, algorithmName, environment) {
+  const dateTime = getCurrentDateTime();
+  const filename = `../../dataset/${algorithmName}_${environment}--${dateTime}.csv`;
+  const header = 'Algorithm,Input,Time\n';
+  const rows = results.map(result => `${algorithmName},${result.input},${result.time}`).join('\n');
+  const csvContent = header + rows;
+
+  try {
+    await Deno.writeTextFile(filename, csvContent);
+    console.log(`Benchmark completado y resultados exportados a ${filename}`);
+  } catch (err) {
+    console.error(`Error al escribir el archivo ${filename}:`, err);
+  }
+}
+
+const inputs = Array.from({ length: 48 }, (_, i) => i);
+
+const results = benchmark(fib, inputs);
+
+exportToCSV(results, 'fibonacci', "deno-rust");
