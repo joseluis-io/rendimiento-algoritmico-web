85433f1cb49cce294ec169b165404d996a341dd9
José Luis de la Hoz <jl@joseluis.io>
2024-11-11 19:52:49 +0100

    Benchmark Node/C++

--- src/node-addons/main.js
+++ src/node-addons/main.js
@@ -1,4 +1,4 @@
-const { fibonacci, linearSearch, binarySearch } = require("./lib/binding.js");
+const { fibonacci, linearSearch, binarySearch, bubbleSort, Queue } = require("./lib/binding.js");
 const fs = require('fs');
 
 function benchmark(algorithm, inputs) {
@@ -8,11 +8,98 @@ function benchmark(algorithm, inputs) {
         algorithm(input);
         const end = performance.now();
         results.push({
-            algorithm: algorithm.name,
+            algorithm: "fibonacci",
             input: input,
             time: end - start
         });
     }
+    return results;
+}
+
+function generateInt32Array(size) {
+    const array = new Int32Array(size);
+    for (let i = 0; i < size; i++) {
+        array[i] = i + 1;
+    }
+    return array;
+}
+
+function benchmarkLinearSearch(linearSearch) {
+    const sizes = [100, 1000, 5000, 10000, 100000, 1000000, 10000000];
+    const results = [];
+    sizes.forEach(size => {
+        const array = generateInt32Array(sizes);
+        const searchValue = size;
+        const start = performance.now();
+        const result = linearSearch(array.buffer, searchValue);
+        const end = performance.now();
+        const duration = end - start;
+        results.push({
+            algorithm: "linearSearch",
+            input: `LinearSearch(Array[${size}])`,
+            result: result,
+            time: duration
+        });
+    });
+    return results;
+}
+
+function benchmarkBinarySearch(binarySearch) {
+    const results = [];
+    const sizes = [100, 1000, 5000, 10000, 100000, 1000000, 10000000];
+    sizes.forEach(size => {
+        const array = generateInt32Array(sizes);
+        const searchValue = size;
+        const start = performance.now();
+        const result = binarySearch(array.buffer, searchValue);
+        const end = performance.now();
+        const duration = end - start;
+        results.push({
+            algorithm: "binarySearch",
+            input: `binarySearch(Array[${size}])`,
+            result: result,
+            time: duration
+        });
+    });
+    return results;
+}
+
+function benchmarkBubbleSort(bubbleSort) {
+    const results = [];
+    const sizes = [100, 1000, 5000, 10000, 100000];
+    sizes.forEach(size => {
+        const array = generateInt32Array(size);
+        const start = performance.now();
+        bubbleSort(array.buffer);
+        const end = performance.now();
+        const duration = end - start;
+        results.push({
+            algorithm: "bubbleSort",
+            input: `bubbleSort(Array[${size}])`,
+            result: array,
+            time: duration
+        });
+    });
+    return results;
+}
+
+function benchmarkQueue() {
+    const queue = new Queue();
+    const sizes = [100, 1000, 5000, 10000, 100000, 1000000, 10000000];
+    const results = [];
+    sizes.forEach(size => {
+        const start = performance.now();
+        for (let i = 0; i < size; i++) {
+            queue.push(i);
+        }
+        const end = performance.now();
+        const duration = end - start;
+        results.push({
+            algorithm: "Queue.push",
+            input: `size[${size}]`,
+            time: duration
+        });
+    });
     return results;
 }
 
@@ -37,8 +124,14 @@ function exportToCSV(results, algorithmName, environment) {
     console.log(`Resultados exportados al fichero: ${filename}`);
 }
 
-const inputs = Array.from({ length: 48 }, (_, i) => i);
+const inputs = Array.from({ length: 20 }, (_, i) => i);
 
-const results = benchmark(fibonacci, inputs);
+// const results = benchmark(fibonacci, inputs);
 
-exportToCSV(results, 'fibonacci', "Node-CPP");
+// exportToCSV(results, 'fibonacci', "Node-CPP");
+
+console.log(benchmark(fibonacci, inputs));
+console.log(benchmarkLinearSearch(linearSearch));
+console.log(benchmarkBinarySearch(binarySearch));
+console.log(benchmarkBubbleSort(bubbleSort));
+console.log(benchmarkQueue());
