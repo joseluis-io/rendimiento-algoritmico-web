4c0996e025595499877402b1d286fd630ec9b8ed
José Luis de la Hoz <jl@joseluis.io>
2024-11-11 19:33:05 +0100

    Benchmark JS

--- src/js/benchmark.js
+++ src/js/benchmark.js
@@ -1,11 +1,12 @@
-import { fibonacci } from './algorithm.js';
+import { binarySearch, bubbleSort, fibonacci, linearSearch, Queue } from './algorithm.js';
 const isNode = typeof process !== 'undefined' && process.versions != null && process.versions.node != null;
 
 let writeFileSync;
 
+
+// import dinámico
 async function setupFileSystem() {
     if (isNode) {
-        // import dinámico
         const fs = await import('fs');
         writeFileSync = fs.writeFileSync;
     }
@@ -23,6 +24,85 @@ function benchmark(algorithm, inputs) {
             time: end - start
         });
     }
+    return results;
+}
+
+function benchmarkLinearSearch(linearSearch) {
+    const sizes = [100, 1000, 5000, 10000, 100000, 1000000, 10000000];
+    const results = [];
+    sizes.forEach(size => {
+        const array = Array.from({ length: size }, (_, i) => i + 1);
+        const searchValue = size;
+        const start = performance.now();
+        const result = linearSearch(array, searchValue);
+        const end = performance.now();
+        const duration = end - start;
+        results.push({
+            algorithm: "linearSearch",
+            input: `LinearSearch(Array[${size}])`,
+            result: result,
+            time: duration
+        });
+    });
+    return results;
+}
+
+function benchmarkBinarySearch(binarySearch) {
+    const results = [];
+    const sizes = [100, 1000, 5000, 10000, 100000, 1000000, 10000000];
+    sizes.forEach(size => {
+        const array = Array.from({ length: size }, (_, i) => i + 1)
+        const searchValue = size;
+        const start = performance.now();
+        const result = binarySearch(array, searchValue);
+        const end = performance.now();
+        const duration = end - start;
+        results.push({
+            algorithm: "binarySearch",
+            input: `binarySearch(Array[${size}])`,
+            result: result,
+            time: duration
+        });
+    });
+    return results;
+}
+
+function benchmarkBubbleSort(bubbleSort) {
+    const results = [];
+    const sizes = [100, 1000, 5000, 10000, 100000];
+    sizes.forEach(size => {
+        const array = Array.from({ length: size }, (_, i) => Math.floor(Math.random() * 10000));
+        const start = performance.now();
+        bubbleSort(array);
+        const end = performance.now();
+        const duration = end - start;
+        results.push({
+            algorithm: "bubbleSort",
+            input: `bubbleSort(Array[${size}])`,
+            result: array,
+            time: duration
+        });
+    });
+    return results;
+}
+
+function benchmarkQueue() {
+    const queue = new Queue();
+    const sizes = [100, 1000, 5000, 10000, 100000, 1000000, 10000000];
+    const results = [];
+    sizes.forEach(size => {
+        const start = performance.now();
+        for (let i = 0; i < size; i++) {
+            queue.push(i);
+        }
+        const end = performance.now();
+        const duration = end - start;
+        results.push({
+            algorithm: "Queue.push",
+            input: `size[${size}]`,
+            time: duration
+        });
+    });
     return results;
 }
 
@@ -56,8 +136,18 @@ async function exportToCSV(results, algorithmName, environment) {
     }
 }
 
-const inputs = Array.from({length: 48}, (_, i) => i);
+const inputs = Array.from({ length: 20 }, (_, i) => i);
 
 const results = benchmark(fibonacci, inputs);
+console.log(results);
 
-exportToCSV(results, 'fibonacci', isNode ? "node" : "browser");
+console.log(benchmarkLinearSearch(linearSearch));
+
+console.log(benchmarkBinarySearch(binarySearch));
+
+console.log(benchmarkBubbleSort(bubbleSort));
+
+console.log(benchmarkQueue());
+
+
+// exportToCSV(results, 'fibonacci', isNode ? "node" : "browser");
