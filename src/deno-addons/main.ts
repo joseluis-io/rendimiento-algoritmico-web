33a2b6bedf609375b9ef6ef3c2d02bdf14350900
José Luis de la Hoz <jl@joseluis.io>
2024-11-11 20:38:43 +0100

    Benchmark Deno/Rust

--- src/deno-addons/main.ts
+++ src/deno-addons/main.ts
@@ -29,6 +29,93 @@ function benchmark(algorithm, inputs) {
   return results;
 }
 
+function generateInt32Array(size) {
+  const array = new Int32Array(size);
+  for (let i = 0; i < size; i++) {
+    array[i] = i + 1;
+  }
+  return array;
+}
+
+function benchmarkLinearSearch(linearSearch) {
+  const sizes = [100, 1000, 5000, 10000, 100000, 1000000, 10000000];
+  const results = [];
+  sizes.forEach(size => {
+      const array = generateInt32Array(sizes);
+      const searchValue = size;
+      const start = performance.now();
+      const result = linearSearch(array, array.length, searchValue);
+      const end = performance.now();
+      const duration = end - start;
+      results.push({
+          algorithm: "linearSearch",
+          input: `LinearSearch(Array[${size}])`,
+          result: result,
+          time: duration
+      });
+  });
+  return results;
+}
+
+function benchmarkBinarySearch(binarySearch) {
+  const results = [];
+  const sizes = [100, 1000, 5000, 10000, 100000, 1000000, 10000000];
+  sizes.forEach(size => {
+      const array = generateInt32Array(sizes);
+      const searchValue = size;
+      const start = performance.now();
+      const result = binarySearch(array, array.length, searchValue);
+      const end = performance.now();
+      const duration = end - start;
+      results.push({
+          algorithm: "binarySearch",
+          input: `binarySearch(Array[${size}])`,
+          result: result,
+          time: duration
+      });
+  });
+  return results;
+}
+
+function benchmarkBubbleSort(bubbleSort) {
+  const results = [];
+  const sizes = [100, 1000, 5000, 10000, 100000];
+  sizes.forEach(size => {
+      const array = generateInt32Array(size);
+      const start = performance.now();
+      bubbleSort(array, array.length);
+      const end = performance.now();
+      const duration = end - start;
+      results.push({
+          algorithm: "bubbleSort",
+          input: `bubbleSort(Array[${size}])`,
+          result: array,
+          time: duration
+      });
+  });
+  return results;
+}
+
+function benchmarkQueue() {
+  const queue = new Queue();
+  const sizes = [100, 1000, 5000, 10000, 100000, 1000000, 10000000];
+  const results = [];
+  sizes.forEach(size => {
+      const start = performance.now();
+      for (let i = 0; i < size; i++) {
+          queue.push(i);
+      }
+      const end = performance.now();
+      const duration = end - start;
+      results.push({
+          algorithm: "Queue.push",
+          input: `size[${size}]`,
+          time: duration
+      });
+  });
+  return results;
+}
+
 // formato: YYYYMMDDTHHMM
 function getCurrentDateTime() {
   const date = new Date();
@@ -55,8 +142,14 @@ async function exportToCSV(results, algorithmName, environment) {
   }
 }
 
-const inputs = Array.from({ length: 48 }, (_, i) => i);
+const inputs = Array.from({ length: 20 }, (_, i) => i);
 
-const results = benchmark(fib, inputs);
+// const results = benchmark(fib, inputs);
 
-exportToCSV(results, 'fibonacci', "deno-rust");
+console.log(benchmark(fib, inputs));
+console.log(benchmarkLinearSearch(linearSearch));
+console.log(benchmarkBinarySearch(binarySearch));
+console.log(benchmarkBubbleSort(bubbleSort));
+console.log(benchmarkQueue());
+
+// exportToCSV(results, 'fibonacci', "deno-rust");
